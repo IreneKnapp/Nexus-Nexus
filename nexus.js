dd66c2271e30c9f2a9788099df0e489622862f79
Irene Knapp <irene.knapp@icloud.com>
2012-11-09 08:01:34 -0500

    Stuff and progress.

--- nexus.js
+++ nexus.js
@@ -1,5 +1,52 @@
 var http = require('http');
+var sql = require('sqlite3');
 var config = require('./config.json');
+
+
+var db = new sql.Database(config.database);
+
+
+function createTable(db, tableName, columns, constraints) {
+    var tableSQL = "CREATE TABLE " + tableName + " (\n";
+    
+    var items = [];
+    
+    for(var i in columns) {
+        var column = columns[i];
+        var columnSQL = "  " + column[0] + " " + column[1];
+        items.push(columnSQL);
+    }
+    
+    for(var i in constraints) {
+        var constraint = constraints[i];
+        var constraintSQL = constraint;
+        items.push(constraintSQL);
+    }
+    
+    for(var i = 0; i < items.length; i++) {
+        var last = (i + 1 == items.length);
+        
+        tableSQL += items[i];
+        
+        if(!last) tableSQL += ",";
+        tableSQL += "\n";
+    }
+    
+    tableSQL += ")";
+    
+    db.run(tableSQL);
+}
+
+
+db.serialize(function() {
+    createTable(db, "images", [
+        ["image_id", "blob primary key"],
+        ["name", "text"],
+        ["data", "blob"]
+    ], []);
+});
+
+
 
 http.createServer(function(request, response) {
   reponse.writeHead(200, {'Content-Type': 'text/plain'});
