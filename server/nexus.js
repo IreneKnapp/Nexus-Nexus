b99f66c36fccc813f4e9dbf9f28bbd21a299c443
Irene Knapp <irene.knapp@icloud.com>
2012-11-18 23:47:54 -0500

    Use direct-schema for real.

--- server/nexus.js
+++ server/nexus.js
@@ -1,23 +1,14 @@
 var http = require("http");
 var fs = require("fs");
 var sql = require("sqlite3");
-var jsv = require("JSV").JSV;
 var config = require("./../config/config.json");
-//var schema = require("./schema.js");
+var schema = require("./schema.js");
 
 var db = new sql.Database(config.database);
 
 
 db.serialize(function() {
-    var metaschema = JSON.parse(fs.readFileSync('schemas/schema.json', "utf8"));
-    var schema = JSON.parse(fs.readFileSync('config/schema.json', "utf8"));
-    var environment = jsv.createEnvironment();
-    var report = environment.validate(schema, metaschema);
-    if(report.errors.length == 0) {
-        console.log("Okay!");
-    } else {
-        console.log(report.errors);
-    }
+    schema.load('config/schema.json');
 });
 
 
